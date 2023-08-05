38a1dbaec7a8841cdb573b6becadddf869ce2cf8
amitsafi45 <safiamit45@gmail.com>
2023-08-05 21:21:37 +0545

    remove: console log

--- auth-server/src/server.ts
+++ auth-server/src/server.ts
@@ -1,17 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
-// import { PrismaClient } from '@prisma/client'
 import { prisma } from './config/database.config'
 const sever=express()
 
-console.log(process.env.DATABASE_URL);
 
-// const prisma = new PrismaClient()
 
 async function main() {
-  const y=await prisma.user.create({data:{username:"lll",email:"ddd@g.com",password:"Admin@123"}})
+ // const y=await prisma.user.create({data:{username:"lll",email:"ddd@g.com",password:"Admin@123"}})
   // ... you will write your Prisma Client queries here
-  console.log(y)
+  //console.log(y)
   sever.listen(process.env.PORT,()=>{
     console.log(`Auth-server listening at ${process.env.PORT}`)
 })
