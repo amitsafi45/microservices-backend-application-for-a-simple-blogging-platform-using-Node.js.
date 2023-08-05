6d56f7fe33e808be0fe06c42e17f285412ea927c
amitsafi45 <safiamit45@gmail.com>
2023-08-06 04:35:42 +0545

    add: some function ti populate reponse message

---
+++ auth-server/src/utils/responseMessage.utils.ts
@@ -0,0 +1,12 @@
+export const getNotFoundMessage = (title: string) => {
+    return `${title} not found`;
+  };
+  export const deletedMessage = (title: string) => {
+    return `${title} deleted successfully`;
+  };
+  export const UpdatedMessage = (title: string) => {
+    return `${title} updated successfully`;
+  };
+  export const CreatedMessage = (title: string) => {
+    return `${title} created successfully`;
+  };
