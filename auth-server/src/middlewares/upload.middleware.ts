e93b1b873ba06d9089d1b47bd080e64967ec95f8
amit safi <amit safi>
2023-08-07 16:35:39 +0545

    add: media ,token

--- auth-server/src/middlewares/upload.middleware.ts
+++ auth-server/src/middlewares/upload.middleware.ts
@@ -17,30 +17,21 @@ import { iocContainer } from '../utils/IoCContainer.utils'
       this.mediaTypes=Object.values(MediaType)
     }
   single(fieldName: string){
-    console.log(fieldName)
     return async (req: Request, res: Response, next: NextFunction) => {
       if (!req.files) {
-        console.log("second")
        return next( HttpException.badRequest('No file uploaded'))
       }
-      // console.log(req.files)
       console.log("first")
-     // *Ensure that only one file is uploaded
       if (Object.keys(req.files).length !== 1) {
        return next( HttpException.badRequest('Only one file can be uploaded'))
       }
-  console.log("third")
       if (!this.mediaTypes.includes(req.body.type)) {
        return next( HttpException.badRequest('Invalid media type'))
       }
       try {
-        console.log(req.files)
-        console.log(fieldName,"fffff")
         const file = req.files[fieldName] as UploadedFile
-         console.log(file,"file")
         const fileName = `${Date.now()}__${randomBytes(3).toString('hex')}__${file.name}`
         let fileData = await this.uploadHelper.help(fileName, file, req.body.type)
-console.log("third")
         req.file = {
           name: fileName,
           type: req.body.type,
@@ -55,5 +46,5 @@ console.log("third")
 
  
 } 
-export default new Upload()
-// iocContainer.register(Upload,new Upload())
+
+iocContainer.register(Upload,new Upload())
--- auth-server/src/routes/media.routes.ts
+++ auth-server/src/routes/media.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import upload from "../middlewares/upload.middleware"
+import {Upload} from "../middlewares/upload.middleware"
 import mediaController from "../controllers/media.controller"
 import { catchAsync } from "../utils/catchAsync"
 import Authentication from "../middlewares/Authentication.middleware"
+import { iocContainer } from "../utils/IoCContainer.utils"
 const router = Router()
 // router.use(Authentication.Check())
-router.post('/upload/single', upload.single('media'), catchAsync(mediaController.uploadSingle.bind(mediaController)))
+router.post('/upload/single', iocContainer.resolve(Upload).single("media"), catchAsync(mediaController.uploadSingle.bind(mediaController)))
 export default router
