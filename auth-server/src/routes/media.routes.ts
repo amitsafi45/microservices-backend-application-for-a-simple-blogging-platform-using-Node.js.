d05681d96c70402852a64742a4ac8bac85e15e4e
amit safi <amit safi>
2023-08-11 13:41:42 +0545

    fix: protect medai route

--- auth-server/src/routes/media.routes.ts
+++ auth-server/src/routes/media.routes.ts
@@ -3,8 +3,9 @@ import {Upload} from "../middlewares/upload.middleware"
 import {MediaController} from "../controllers/media.controller"
 import { catchAsync } from "../utils/catchAsync"
 import { container } from "tsyringe"
+import Authentication from "../middlewares/Authentication.middleware"
 const router = Router()
 const iocMediaContainer=container.resolve(MediaController)
 const iocUploadContainer=container.resolve(Upload)
-router.post('/upload/single', iocUploadContainer.single("media").bind(iocUploadContainer), catchAsync(iocMediaContainer.uploadSingle.bind(iocMediaContainer)))
+router.post('/upload/single',Authentication.Check(), iocUploadContainer.single("media").bind(iocUploadContainer), catchAsync(iocMediaContainer.uploadSingle.bind(iocMediaContainer)))
 export default router
