ec5ecebbfdfbadec44cd7b43813047cbcdd2267d
amit safi <amit safi>
2023-08-11 16:43:04 +0545

    fix: issue media path

--- blogging-server/src/utils/response.ts
+++ blogging-server/src/utils/response.ts
@@ -5,11 +5,11 @@
 export class ReturnResponse<T> {
   data?:T
   list?:T[]
-    status:string;
+    success:boolean;
     code:number;
     message:string; 
     constructor(
-      status:string,
+      success:boolean,
       code:number,
       message:string,
        data?:T,
@@ -17,18 +17,18 @@ export class ReturnResponse<T> {
     ) {
       this.data=data
       this.list=list
-      this.status=status;
+      this.success=success;
       this.code=code;
       this.message=message;
   }
  
 }
 export function createResponse<T>(
-  status: string,
+  success: boolean,
   code: number,
   message: string,
   data?: T,
   list?: T[]
 ): ReturnResponse<T> {
-  return new ReturnResponse<T>(status, code, message, data, list);
+  return new ReturnResponse<T>(success, code, message, data, list);
 }
