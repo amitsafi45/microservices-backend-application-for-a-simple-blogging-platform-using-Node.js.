eb1cce04d03607acc3df73d8e0851072382e5374
amitsafi45 <safiamit45@gmail.com>
2023-08-13 22:10:18 +0545

    fix: register service

--- blogging-server/src/server.ts
+++ blogging-server/src/server.ts
@@ -1,22 +1,66 @@
-import 'reflect-metadata'
-import express from 'express'
-import middleware from './middlewares';
-import EnvironmentConfiguration from './config/env.config';
-async function bootStrap() {
-  const app = express();
+import express, { Request, Response, response } from "express";
+import "reflect-metadata";
+import middleware from "./middlewares";
+import EnvironmentConfiguration from "./config/env.config";
+import axios from "axios";
+import { error } from "console";
 
+const app = express();
+async function bootStrap() {
   await middleware(app);
-  app.listen(EnvironmentConfiguration.PORT, async () => {
-   
-      console.info(
-        `Server started at http://localhost:${EnvironmentConfiguration.PORT}`
-      );
+  app.listen(EnvironmentConfiguration.PORT || 3000, async () => {
+    console.info(
+      `Blogging Server started at http://localhost:${EnvironmentConfiguration.PORT}`
+    );
+    const registerService = async () =>
+      await axios.post("http://localhost:4000/service-registry/api/register", {
+        clientName: "Blogging Section",
+        host: "localhost",
+        port: EnvironmentConfiguration.PORT,
+        status: "LIVE",
+        serviceName: "blogging",
+      });
+    const updateService = async (status: string) =>
+      await axios.patch("http://localhost:4000/service-registry/api/register", {
+        clientName: "blogging Section",
+        host: "localhost",
+        port: EnvironmentConfiguration.PORT,
+        status: status,
+        serviceName: "blogging",
+      });
+    registerService()
+      .then(async (response) => {
+        console.log(response.data);
+      })
+      .catch(async (error) => {
+        if (error.response.status === 409) {
+          await updateService("LIVE");
+        }
+      });
+
+    const interval = setInterval(updateService, 5*60*1000);
+    const cleanUp = async () => {
+      clearInterval(interval);
+      await updateService("DIE");
+    }
+    process.on("uncaughtException",async()=>{
+      await cleanUp();
+      process.exit()
+          })
+    process.on('SIGINT',async()=>{
+      await cleanUp()
+      process.exit()
+    })
+   process.on('SIGTERM',async()=>{
+    await cleanUp()
+    process.exit()
+   })
   });
 }
+
 try {
   bootStrap();
 } catch (error) {
   console.log(error);
   process.exit(1);
 }
-
