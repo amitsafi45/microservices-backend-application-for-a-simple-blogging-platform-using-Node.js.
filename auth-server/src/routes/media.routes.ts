a6377ebe00d2d9a71a291b59f53e3099a1476bf7
amit safi <amit safi>
2023-08-13 16:37:30 +0545

    fix: protect from invalid route

--- auth-server/src/routes/media.routes.ts
+++ auth-server/src/routes/media.routes.ts
@@ -4,8 +4,12 @@ import {MediaController} from "../controllers/media.controller"
 import { catchAsync } from "../utils/catchAsync"
 import { container } from "tsyringe"
 import Authentication from "../middlewares/Authentication.middleware"
+import HttpException from "../utils/HttpException"
 const router = Router()
 const iocMediaContainer=container.resolve(MediaController)
 const iocUploadContainer=container.resolve(Upload)
 router.post('/upload-single',iocUploadContainer.single("media").bind(iocUploadContainer), catchAsync(iocMediaContainer.uploadSingle.bind(iocMediaContainer)))
+router.all('/*',(req,res)=>{
+    throw HttpException.notFound("Method Not Found  ")
+  })
 export default router
