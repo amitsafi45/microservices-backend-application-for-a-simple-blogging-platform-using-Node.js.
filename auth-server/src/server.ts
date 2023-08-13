8dc5cc86d28af61ab4447c0798f944960c3eef40
amitsafi45 <safiamit45@gmail.com>
2023-08-13 22:10:27 +0545

    fix: register service

--- auth-server/src/server.ts
+++ auth-server/src/server.ts
@@ -1,137 +1,66 @@
-import express, { Request, Response } from 'express'
-import 'reflect-metadata';
-import middleware from './middlewares';
-import EnvironmentConfiguration from './config/env.config';
-import axios from 'axios';
-import { error } from 'console';
+import express, { Request, Response, response } from "express";
+import "reflect-metadata";
+import middleware from "./middlewares";
+import EnvironmentConfiguration from "./config/env.config";
+import axios from "axios";
+import { error } from "console";
 
-//   console.log('an event occurred!');
-// });
-// myEmitter.emit('event');
 const app = express();
 async function bootStrap() {
-
   await middleware(app);
-  app.listen(EnvironmentConfiguration.PORT ||3000, async () => {
-   
-      console.info(
-        `Auth Server started at http://localhost:${EnvironmentConfiguration.PORT}`
-      );
-      // const registerService=async()=> await axios.post('http://localhost:4000/service-registry/api/register',{
-      //   "clientName":"amit auth supplier",
-      //   "host":"localhost",
-      //   "port":EnvironmentConfiguration.PORT,
-      //   "status":"LIVE",
-      //   "serviceName":"auth"        
-      // })
-      // await registerService()
-      // console.log("dddd");
+  app.listen(EnvironmentConfiguration.PORT || 3000, async () => {
+    console.info(
+      `Auth Server started at http://localhost:${EnvironmentConfiguration.PORT}`
+    );
+    const registerService = async () =>
+      await axios.post("http://localhost:4000/service-registry/api/register", {
+        clientName: "Authentication Section",
+        host: "localhost",
+        port: EnvironmentConfiguration.PORT,
+        status: "LIVE",
+        serviceName: "auth",
+      });
+    const updateService = async (status: string) =>
+      await axios.patch("http://localhost:4000/service-registry/api/register", {
+        clientName: "Authentication Section",
+        host: "localhost",
+        port: EnvironmentConfiguration.PORT,
+        status: status,
+        serviceName: "auth",
+      });
+    registerService()
+      .then(async (response) => {
+        console.log(response.data);
+      })
+      .catch(async (error) => {
+        if (error.response.status === 409) {
+          await updateService("LIVE");
+        }
+      });
 
-
-
-
-
-
-
-
-
-      //  const interval=setInterval(registerService,20*1000);
-      //  const cleanUp=async()=>{
-      //    clearInterval(interval);
-      //  }
-
-    //   const register=()=>console.log("first heelll")
-
-    //   const pingMainServer= ()=>{
-    //    axios.get('http://localhost:4000/metro/api/ping').then((response)=>{
-    //         console.log("response",response.data)
-              
-    //    }).catch((error)=>{
-    //     // console.log("dddd")
-    //       console.log(error.message)
-    //    })
-    // }
-
-    // setInterval(pingMainServer,5000)
-//      myEmitter.on('ping main server', async() => {
-//       try{
-//         const registerService= await axios.get('http://localhost:4000/service-registry/api/ping')    
-//           console.log(registerService.data);
-//       }catch(e:any){
-//         console.log(e.message);
-//       }
-//     });
-//       myEmitter.emit('ping main server');
-//   });
-  // const registerService=async()=> await axios.post('http://localhost:4000/service-registry/api/register',{
-  //     "clientName":"amit auth supplier",
-  //     "host":"localhost",
-  //     "port":EnvironmentConfiguration.PORT,
-  //     "status":"LIVE",
-  //     "serviceName":"auth"        
-  //   })
-  //   registerService().then((response)=>console.log(response.data,"ggg")).catch((error)=>console.log(error,"fff"))
-  //  console.log("dddd");
-//   // app.on('listen',async()=>{
-//   //   console.log("object");
-//   //     const registerService=async()=> await axios.post('http://localhost:4000/service-registry/api/register',{
-//   //       "clientName":"amit auth supplier",
-//   //       "host":"localhost",
-//   //       "port":EnvironmentConfiguration.PORT,
-//   //       "status":"LIVE",
-//   //       "serviceName":"auth"        
-//   //     })
-//   //    await registerService()
-//   //    console.log("dddd");
-//   //     const interval=setInterval(registerService,20*1000);
-//       // const cleanUp=async()=>{
-//       //   clearInterval(interval);
-//       //   await 
-//       // }
-
-
- })
-
- }
- 
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const interval = setInterval(updateService, 7*60*1000);
+    const cleanUp = async () => {
+      clearInterval(interval);
+      await updateService("DIE");
+    }
+    process.on("uncaughtException",async()=>{
+      await cleanUp();
+      process.exit()
+          })
+    process.on('SIGINT',async()=>{
+      await cleanUp()
+      process.exit()
+    })
+   process.on('SIGTERM',async()=>{
+    await cleanUp()
+    process.exit()
+   })
+  });
+}
 
 try {
-   bootStrap();
-
+  bootStrap();
 } catch (error) {
   console.log(error);
   process.exit(1);
 }
-
-    
-    // const interval=setInterval(registerService,20*1000)
